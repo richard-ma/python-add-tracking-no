5631754a3bc65204433fd5a1ec101430dc36e9de
Richard Ma <richard.ma.19850509@gmail.com>
2019-11-01 13:51:02 +0800

    alpha version

--- add-tracking-no.py
+++ add-tracking-no.py
@@ -71,24 +71,26 @@ class RecordDataFile(DataFile):
         self.filename = filename
 
     def write(self, data):
+        #print(data)
         with open(self.filename, 'w+', encoding='GBK') as f:
             w = csv.writer(f)
             w.writerows(data)
 
 if __name__ == "__main__":
-    #ship_file = ShipDataFile("test_ship.csv")
-    #data = ship_file.read()
-    #print(data)
-    #ship_file = ShipDataFile("only_in_ship.csv")
-    #write_file.write(data)
-
-    #express_file = ExpressDataFile("test_express.xlsx")
-    #ws = express_file.read().active
-    #print(ws['A28'].value)
 
     # 获取文件名
-    ship_filename = "test_ship.csv" # *_ship.csv
-    express_filename = "test_express.xlsx" # *_express.xlsx
+    # ship_filename
+    ship_filename = os.path.basename(input("*_ship.csv filename?")) # *_ship.csv
+    if ship_filename[-2:] == ('\' '):
+        ship_filename = ship_filename[:-2]
+    #print(ship_filename)
+
+    # express_filename
+    express_filename = os.path.basename(input("*_express.xlsx filename?")) # *_express.xlsx
+    if express_filename[-2:] == ('\' '):
+        express_filename = express_filename[:-2]
+    #print(express_filename)
+
     backup_express_filename = "copy_" + express_filename # copy_ + express_filename
     only_in_ship_filename = "only_in_" # only_in_*_ship.txt
     only_in_express_filename = "only_in_" # only_in_*_express.txt
@@ -97,11 +99,11 @@ if __name__ == "__main__":
     # ship file
     ship_file = ShipDataFile(ship_filename)
     ship_file_data = ship_file.read() # {"订单标识":"商品交易号"}
-    print(ship_file_data)
+    #print(ship_file_data)
     #express file
     express_file = ExpressDataFile(express_filename)
     ws = express_file.read().active # get active worksheet
-    print(ws["A1"].value)
+    #print(ws["A1"].value)
 
     # 备份原文件
     copy(express_filename, backup_express_filename)
@@ -109,17 +111,36 @@ if __name__ == "__main__":
     # 查找修改数据
     interval = 13
     i = 2
+    used_ship_data = set()
+    unfound_express_data = list()
     while ws["A%d" % (i)].value != None:
         k = str(ws["A%d" % (i)].value)
         if k in ship_file_data.keys():
+            used_ship_data.add(k)
             ws["J%d" % (i)] = ship_file_data[k] # add tracking no
+        else:
+            unfound_express_data.append(k)
         i += interval
 
     # 保存文件
     express_file.write()
 
     # 写入记录
+    #print(used_ship_data)
+    #print(ship_file_data.keys())
+    #print(unfound_express_data)
+
     only_in_ship_filename += ship_file.prefix + "_ship.txt"
-    print(only_in_ship_filename)
+    #print(only_in_ship_filename)
+    only_in_ship_file = RecordDataFile(only_in_ship_filename)
+    #print(set(ship_file_data.keys()).difference(used_ship_data))
+    data = [[x] for x in list(set(ship_file_data.keys()).difference(used_ship_data))]
+    #print(data)
+    only_in_ship_file.write(data)
+
     only_in_express_filename += express_file.prefix + "_express.txt"
-    print(only_in_express_filename)
+    #print(only_in_express_filename)
+    only_in_express_file = RecordDataFile(only_in_express_filename)
+    data = [[x] for x in unfound_express_data]
+    #print(data)
+    only_in_express_file.write(data)
